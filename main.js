b394316b0cbf2d270544d5763a9e0d5339c4354d
Wowannygo <mail@gmail.com>
2023-12-26 20:17:11 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+let firstName = 'Anna'
+let myAge = '18'
+let myCity = 'KJA'
+let myHobby = 'snowboarding, painting and watching movies'
+
+console.log('Hello, I am', firstName)
+console.log('I am', myAge)
+console.log('I am from', myCity)
+console.log('My hobbies are', myHobby)

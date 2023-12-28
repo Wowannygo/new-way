af2f54ef7ebc1393ce7955e4ec5f9f972b7dce69
Wowannygo <mail@gmail.com>
2023-12-28 22:49:07 +0600

    "Lesson 02"

---
+++ JS2.0/type.js
@@ -0,0 +1,13 @@
+let titleProject = 'Оценка стоимости';
+let screensValue = 'шаблонные, с уникальным дизайном, с анимациями';
+let screenPrice = '6000';
+let percentage = '3';
+let fullPrice = '100000';
+let responsive = 'true';
+
+
+console.log(typeof titleProject);
+console.log(typeof screensValue);
+console.log(typeof screenPrice);
+console.log(typeof percentage);
+console.log(typeof fullPrice);

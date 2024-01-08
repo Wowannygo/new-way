1840fb5d72bf7c1bd89884aab18b4045e2aab536
Wowannygo <mail@gmail.com>
2024-01-08 17:24:09 +0600

    JS4.0 update

--- JS3.0/project.js
+++ JS3.0/project.js
@@ -2,12 +2,6 @@ let titleProject = prompt('Название проекта?')
 console.log(titleProject);
 let screensValue = prompt('шаблонные, с уникальным дизайном, с анимациями')
 console.log(screensValue);
-function getTitle() {
-    return titleProject.charAt(0).toUpperCase() +
-        titleProject.slice(1)
-}
-getTitle();
-console.log(getTitle(dada));
 
 let responsive = confirm('Нужен ли респонсивный сайт?')
 console.log(responsive)
@@ -40,9 +34,3 @@ if (fullPrice > 50000) {
 } else if (fullPrice < 0) {
     alert('что-то пошло не так')
 }
-
-const allServicePrices = function getAllServicePrices() {
-    return 3 + 2;
-};
-allServicePrices();
-console.log(allServicePrices)
--- JS4.0/func.js
+++ JS4.0/func.js
@@ -1,19 +1,49 @@
+let titleProject = prompt('Название проекта?');
+let screensValue = prompt('шаблонные, с уникальным дизайном, с анимациями')
+console.log(screensValue);
+function getTitle() {
+  return titleProject.charAt(0).toUpperCase() +
+    titleProject.slice(1).toLowerCase();
+}
+console.log('Название проекта', getTitle());
+
+
+let responsive = confirm('Нужен ли респонсивный сайт?')
+
+
+let screenPrice = 1000
+let service1 = prompt('Какой сервис нужен?')
+let servicePrice1 = prompt('Сколько это будет стоить?')
+let service2 = prompt('Какой еще сервис тебе нужен?')
+let servicePrice2 = prompt('Сколько будет стоить этот второй сервис?')
+let fullPrice = screenPrice + +servicePrice1 + +servicePrice2
+console.log(fullPrice)
 const getAllServicePrices = function (servicePrice1, servicePrice2) {
-    return servicePrice1 + servicePrice2;
+  return servicePrice1 + servicePrice2;
 };
 const allServicePrices = getAllServicePrices()
+console.log('Все сервисы', allServicePrices);
 
-
-function getFullPrice() {
-    const result = screenPrice + allServicePrices;
-    return result;
+let Percent = fullPrice * 10 / 100
+function getServicePercentPrices() {
+  return fullPrice - Percent
 }
-const fullPrice = getFullPrice();
-console.log(fullPrice)
+let servicePercentPrice = getServicePercentPrices()
+console.log('Итог', Math.round(servicePercentPrice))
 
-function getTitle() {
-    return titleProject.charAt(0).toUpperCase() +
-        titleProject.slice(1)
+function getRollbackMessage() {
+  if (fullPrice > 50000) {
+    console.log('сделаем скидку в 10%')
+  } else if (20000 < fullPrice && fullPrice < 50000) {
+    console.log('сделаем скидку в 5%')
+  } else if (0 < fullPrice && fullPrice < 20000) {
+    console.log('скидок нет')
+  } else if (fullPrice === 0, 20000, 50000) {
+    console.log('скидка')
+  } else if (fullPrice < 0) {
+    alert('что-то пошло не так')
+  }
 }
-getTitle();
-console.log(getTitle(dada));
+getRollbackMessage();
+
+
